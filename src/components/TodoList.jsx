72c847941438596b7a394ed7f7875dbdd239d269
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2025-07-18 12:02:01 +0300

    Add TodoList component to render multiple TodoCard components

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import TodoCard from './TodoCard'
+const todoList = () => {
+  return (
+    <div>
+      
+      <TodoCard/>
+      <TodoCard/>
+      <TodoCard/>
+    </div>
+  )
+}
+
+export default todoList
