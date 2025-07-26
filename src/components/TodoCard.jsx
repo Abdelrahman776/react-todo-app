0b384921067eed2aa95286eb630b9faa533b5cee
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2025-07-26 16:00:59 +0300

    Implement TodoCard component to display todo items with complete and delete functionality

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
-const TodoCard = () => {
+function TodoCard (props) {
+const { todo, unfilteredTodoIndex, handelCompleteTodo, handelDeleteTodo } =
+  props;
+
   return (
-    <div>TodoCard</div>
-  )
+    <div className="card">
+      {todo.input}
+
+      <button
+        className="button-card"
+        disabled={todo.complete}
+        onClick={() => {
+          handelCompleteTodo(unfilteredTodoIndex);
+        }}
+      >
+        <h6>Complete</h6>
+      </button>
+      <button
+        className="button-card"
+        onClick={() => {
+          handelDeleteTodo(unfilteredTodoIndex);
+        }}
+      >
+        <h6>Delete</h6>
+      </button>
+    </div>
+  );
 }
+
+
+
+
 
 export default TodoCard
