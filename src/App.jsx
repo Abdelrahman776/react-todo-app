0a3462184da86de810ed6a1b32a58b217747f9cf
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2025-07-26 15:56:15 +0300

    Add handler function to complete ,delete and add todo items

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,36 @@ import TodoInput from "./components/TodoInput";
 function App() {
 
   const todos = [
-    { input: "Go to the Gym", complete: false },
-    { input: "First TODO item", complete: true },
     { input: "First TODO item", complete: true },
     { input: "Go to the Gym", complete: false },
+    { input: "eat food ", complete: true },
+    { input: "Study for exams", complete: false },
   ];
+
+
+function handelCompleteTodo(todoIndex) {
+  
+}
+function handelDeleteTodo(todoIndex) {
+  
+}
+function handelEditTodo(todoIndex) {
+  
+}
+function handelSaveTodos() {
+  
+}
 
   return (
     <>
       <Header todos={todos} />
       <Tabs todos={todos} />
-      <TodoList todos={todos} />
-      <TodoInput />
+      <TodoList
+        todos={todos}
+        handelCompleteTodo={handelCompleteTodo}
+        handelDeleteTodo={handelDeleteTodo}
+      />
+      <TodoInput todos={todos} />
     </>
   );
 }
