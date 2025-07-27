164f06d9443564703802fdad3d15a000e074d6df
Abdelrahman kamal <abdelrahmankamal776@gmail.com>
2025-07-27 15:35:44 +0300

    refactor: update TodoCard layout for improved structure and semantics

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -10,33 +10,35 @@ const {
 } = props;
 
   return (
-    <div className="card">
-      <span 
+    <div className="card todo-item">
+      <p
         className="todoInputText"
         onClick={() => {
           handelEditTodo(unfilteredTodoIndex);
         }}
       >
         {todo.input}
-      </span>
+      </p>
 
-      <button
-        className="button-card"
-        // disabled={todo.complete}
-        onClick={() => {
-          handelCompleteTodo(unfilteredTodoIndex);
-        }}
-      >
-        <h6>{!todo.complete ? "Complete" : "Uncomplete"}</h6>
-      </button>
-      <button
-        className="button-card"
-        onClick={() => {
-          handelDeleteTodo(unfilteredTodoIndex);
-        }}
-      >
-        <h6>Delete</h6>
-      </button>
+      <div className="todo-buttons">
+        <button
+          className="button-card"
+          // disabled={todo.complete}
+          onClick={() => {
+            handelCompleteTodo(unfilteredTodoIndex);
+          }}
+        >
+          <h6>{!todo.complete ? "Complete" : "Uncomplete"}</h6>
+        </button>
+        <button
+          className="button-card"
+          onClick={() => {
+            handelDeleteTodo(unfilteredTodoIndex);
+          }}
+        >
+          <h6>Delete</h6>
+        </button>
+      </div>
     </div>
   );
 }
